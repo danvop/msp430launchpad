bac82a16942eab266caa16662cc06510b72f00fa
danvop <danvop@gmail.com>
2015-01-16 00:00:32 +1000

    'lab3 completed'

--- lab3/main.c
+++ lab3/main.c
@@ -3,7 +3,24 @@
 #define TIMER0_A1_VECTOR TIMERA1_VECTOR
 #define TIMER0_A0_VECTOR TIMERA0_VECTOR
 #endif
-void main(void)
-{
-// code goes here
+
+void main(void){
+	WDTCTL = WDTPW + WDTHOLD;
+
+	P1DIR = 0x40;
+	P1OUT = 0;
+
+	BCSCTL3 |= LFXT1S_2;
+	IFG1 &= ~OFIFG;
+	_bis_SR_register(SCG1 + SCG0);
+	BCSCTL2 |= SELM_3 + DIVM_3;
+	while(1){
+		P1OUT = 0x40; // LED on
+		_delay_cycles(100);
+		P1OUT = 0; // LED off
+		_delay_cycles(5000);
+	}
+
+
+	// code goes here
 }
