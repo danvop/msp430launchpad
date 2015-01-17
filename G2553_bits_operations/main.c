41e1f4205870cc478ac7232c3f8c334900836b3e
danvop <danvop@gmail.com>
2015-01-17 19:58:50 +1000

    testing button 1.3 pullup

--- G2553_bits_operations/main.c
+++ G2553_bits_operations/main.c
@@ -1,10 +1,36 @@
-#include <msp430.h> 
+#include <msp430g2553.h>
 
-/*
- * main.c
- */
+#define grn_led	BIT6
+#define red_led BIT0
+#define BTN		BIT3
+
+
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	unsigned int count;
+
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+	P1OUT = 0;
+	P1DIR = grn_led | red_led; //led pins to out
+
+	P1REN |= BTN; //enable p1.3 pullup
+	P1OUT |= BTN; //p1.3 pullup
+
+
+
+	while((P1IN & BTN) == BTN);
+
+	while(1){
+		while((P1IN & BTN) == BTN){
+			P1OUT ^= grn_led;
+			for (count = 0; count < 60000; count++);
+			for (count = 0; count < 60000; count++);
+			for (count = 0; count < 60000; count++);
+		}
+   }
+
+
+
+
 	return 0;
 }
